0e248a91f4dcc0d17f3e4d8d73902a96d7e5f285
johannymk7 <johannymk7@github.com>
2023-11-11 20:11:53 +0100

    Añadir ejercicio-12

--- src/ejercicio-12/index.js
+++ src/ejercicio-12/index.js
@@ -1,1 +1,11 @@
 // Escribe tu código aquí:
+// Declarar la variable fuera del bloque
+let variableBloque;
+
+if (true) {
+  // Asignar un valor dentro del bloque
+  variableBloque = 'Variable dentro del bloque';
+}
+
+// Acceder a la variable fuera del bloque
+console.log(variableBloque); // Generará un error, variableBloque no está definida fuera del bloque
